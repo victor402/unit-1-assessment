144e2dd152f55841107616196da04e74b467e09e
victor <victor@victors-MacBook-Air.local>
2023-01-04 18:36:03 -0500

    second commit

--- main.js
+++ main.js
@@ -2,23 +2,27 @@
  let btnAdd = document.querySelector('#add');
  let btnSub = document.querySelector('#substract');
  let input = document.querySelector('#input');
- let result = parseInt(document.querySelector('#result').innerText);
+ //let result = parseInt(document.querySelector('#result').innerText);
  
 
  btnAdd.addEventListener('click',() =>{
     let b = parseInt(document.querySelector("input").value);
-    let calculate = 0;
-    calculate = b+result;
+    let result = parseInt(document.querySelector('#result').innerText);
+    //let calculate = 0;
+    result += b;
+    //calculate = b+result;
     console.log(b);
-    console.log(result);
-  result = calculate;
+    //console.log(result);
+  
 
- document.getElementById('result').innerHTML = calculate;
+ document.getElementById('result').innerHTML = result;
  });
- btnSub.addEventListener('click',() =>{
-    let b = parseInt(document.querySelector("input").value);
-    let calculate;
-    calculate = b-result;
+  btnSub.addEventListener('click',() =>{
+   let b = parseInt(document.querySelector("input").value);
+   let result = parseInt(document.querySelector('#result').innerText);
+//     let calculate;
+//     calculate = b-result;
+    result -= b;
 
- document.getElementById('result').innerHTML = calculate;
+ document.getElementById('result').innerHTML = result;
 });
