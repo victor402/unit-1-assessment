ff87594348504f8be1c425067e67902647ad7cf8
victor <victor@victors-MacBook-Air.local>
2023-01-05 19:29:31 -0500

    last commit

--- main.js
+++ main.js
@@ -2,26 +2,18 @@
  let btnAdd = document.querySelector('#add');
  let btnSub = document.querySelector('#substract');
  let input = document.querySelector('#input');
- //let result = parseInt(document.querySelector('#result').innerText);
  
-
  btnAdd.addEventListener('click',() =>{
     let b = parseInt(document.querySelector("input").value);
     let result = parseInt(document.querySelector('#result').innerText);
-    //let calculate = 0;
+    
     result += b;
-    //calculate = b+result;
-    console.log(b);
-    //console.log(result);
-  
-
- document.getElementById('result').innerHTML = result;
+    document.getElementById('result').innerHTML = result;
  });
   btnSub.addEventListener('click',() =>{
    let b = parseInt(document.querySelector("input").value);
    let result = parseInt(document.querySelector('#result').innerText);
-//     let calculate;
-//     calculate = b-result;
+
     result -= b;
 
  document.getElementById('result').innerHTML = result;
